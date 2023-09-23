55429ab263086b2adb6c46bf7b5a0e02d2b93ce8
Javier Beneito Barquero <malsolo.com@outlook.com>
2023-09-23 10:12:03 +0200

    Spring Batch learning: spring batch getting started.
    It worked!
    
    No @EnableBatchProcessing though.

--- spring-batch-getting-started/src/main/java/com/malsolo/springframework/batch/gettingstarted/SpringBatchGettingStartedApplication.java
+++ spring-batch-getting-started/src/main/java/com/malsolo/springframework/batch/gettingstarted/SpringBatchGettingStartedApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBatchGettingStartedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchGettingStartedApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchGettingStartedApplication.class, args)));
 	}
 
 }
