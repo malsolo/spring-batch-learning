415a2343d88e72257a4466e5e0eaece5a1d03ad6
Javier Beneito Barquero <malsolo.com@outlook.com>
2023-10-03 08:15:34 +0200

    Spring Batch learning: Batch file -> DB.
    Project from start.spring.io.

---
+++ batch-file-db-processing/src/test/java/com/malsolo/springframework/batch/batchfiledbprocessing/BatchFileDbProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malsolo.springframework.batch.batchfiledbprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchFileDbProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
